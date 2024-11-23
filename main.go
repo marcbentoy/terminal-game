5b9989b1d60381b7e5304ed4acf41532d5ba8447
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-23 13:15:47 +0800

    test cursor

--- main.go
+++ main.go
@@ -23,7 +23,8 @@ func main() {
 		log.Fatal("error initializing screen:", err)
 	}
 
-	player := NewSprite('v', 10, 10)
+	player := NewSprite('.', 10, 10)
+	player2 := NewSprite('.', 10, 11)
 
 	ticker := time.NewTicker(time.Second)
 	doneTicking := make(chan bool)
@@ -37,24 +38,37 @@ func main() {
 			case _ = <-ticker.C:
 				if prevColor == 0 {
 					player.Color = tealColor
+					player2.Color = blueColor
 					prevColor = 1
 				} else {
 					player.Color = blueColor
+					player2.Color = tealColor
 					prevColor = 0
 				}
 
-				screen.Clear()
-				player.Draw(screen)
-				screen.Show()
+				// screen.Clear()
+				// player.Draw(screen)
+				// player2.Draw(screen)
+				// screen.Show()
 			}
 		}
 	}()
+
+	width, height := screen.Size()
+	// selectedChar := '.'
+	cursor := Cursor{
+		X:    width / 2,
+		Y:    height / 2,
+		Char: ' ',
+	}
 
 	running := true
 	for running {
 		screen.Clear()
 
 		player.Draw(screen)
+		player2.Draw(screen)
+		screen.SetContent(cursor.X, cursor.Y, cursor.Char, nil, tcell.StyleDefault.Background(tcell.ColorSeaGreen))
 
 		screen.Show()
 
@@ -62,14 +76,15 @@ func main() {
 		switch ev := ev.(type) {
 		case *tcell.EventKey:
 			switch ev.Rune() {
-			case 'j':
-				player.Y += 1
-			case 'k':
-				player.Y -= 1
-			case 'h':
-				player.X -= 1
-			case 'l':
-				player.X += 1
+			case ' ':
+			case 'j', 's':
+				cursor.Y += 1
+			case 'k', 'w':
+				cursor.Y -= 1
+			case 'h', 'a':
+				cursor.X -= 1
+			case 'l', 'd':
+				cursor.X += 1
 			case 'q':
 				ticker.Stop()
 				doneTicking <- true
